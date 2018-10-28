7b993411b0d67cdfaa8770edfc9130a5eb9aaaeb
katherinestout <kjs14@hood.edu>
2018-10-28 16:53:14 -0400

    name

--- src/NinjaList.js
+++ src/NinjaList.js
@@ -11,12 +11,14 @@ class NinjaList extends Component {
       { name: 'Leo', age: 25, belt: 'magenta',force: '30', id: 3 },
       { name: 'Snoop Dog', age: 250, belt: 'green',force: '300', id: 4 },
       { name: 'Obama', age: 45, belt: 'America',force: '85', id: 5 }
-    ]
+    ],
+    selectNinja: ''
     
   }}
 
   handleClick = name => {
     console.log(name);
+    //setstate
   }
   renderNinjas = ninja => 
   <Ninjas key = {ninja.name} ninja={ninja} onClick = {this.handleClick}/>
@@ -26,7 +28,7 @@ class NinjaList extends Component {
     return (
       <div className="NinjaList">
         <h1>The Ninja Game</h1>
-        <p>Welcome to the dojo, select your ninja: </p>
+        <p>Welcome to the dojo, select your ninja: {} </p>
         <ul>
         {this.state.ninjas.map(this.renderNinjas)}
   
